0ce7d26849d820bc812e4cc92a0e0b4731f1c351
huzaifa-salih <hodaeyfha@gmail.com>
2022-09-23 10:10:19 +0300

    added the model manager to callculate total

--- src/carts/models.py
+++ src/carts/models.py
@@ -1,12 +1,27 @@
 from django.conf import settings
 from django.db import models
 from products.models import Product
-
+from django.db.models.signals import pre_save, m2m_changed, post_save
 
 User = settings.AUTH_USER_MODEL
 
 
 class CartManager(models.Manager):
+    def new_or_get(self, request):
+        cart_id = request.session.get("cart_id", None)
+        qs = self.get_queryset().filter(id=cart_id)
+        if qs.count() == 1:
+            new_obj = False
+            cart_obj = qs.first()
+            if request.user.is_authenticated and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = Cart.objects.new(user=request.user)
+            new_obj = True
+            request.session["cart_id"] = cart_obj.id
+        return cart_obj, new_obj
+
     def new(self, user=None, product=None):
         print(user)
         user_obj = None
@@ -18,8 +33,9 @@ class CartManager(models.Manager):
 
 class Cart(models.Model):
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
-    product = models.ManyToManyField(Product, blank=True)
+    products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    sub_total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -27,3 +43,25 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+####################################################### Signals ################################################################
+def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
+    if action == "post_add" or action == "post_remove" or action == "post_clear":
+        products = instance.products.all()
+        total = 0
+        for x in products:
+            total += x.price
+        print(total)
+        instance.sub_total = total
+        instance.save()
+
+
+m2m_changed.connect(m2m_changed_cart_receiver, sender=Cart.products.through)
+
+
+def pre_save_cart_receiver(sender, instance, *args, **kwargs):
+    instance.total = instance.sub_total
+
+
+pre_save.connect(pre_save_cart_receiver, sender=Cart)
